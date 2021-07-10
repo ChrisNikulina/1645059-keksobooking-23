dd86c20fcbaafc49c8918c422a9a1fceb6b553d3
ChrisNikulina <77628057+ChrisNikulina@users.noreply.github.com>
2021-07-10 19:12:17 +0300

    внесла правки

--- js/form.js
+++ js/form.js
@@ -1,18 +1,24 @@
 const adForm = document.querySelector('.ad-form');
 const formFieldsets = adForm.querySelectorAll('fieldset');
 const mapFilters = document.querySelector('.map__filters');
+const mapFiltersSelect = mapFilters.querySelectorAll('select');
+const mapFiltersFieldset = mapFilters.querySelector('fieldset');
 
-const getInactiveState = () => {
+const setInactiveState = () => {
   adForm.classList.add('ad-form--disabled');
   formFieldsets.forEach((fieldset) => fieldset.disabled = true);
   mapFilters.classList.add('.map__filters--disabled');
+  mapFiltersSelect.forEach((select) => select.disabled = true);
+  mapFiltersFieldset.disabled = true;
 };
 
-const getActiveState = () => {
+const setActiveState = () => {
   adForm.classList.remove('ad-form--disabled');
   formFieldsets.forEach((fieldset) => fieldset.disabled = false);
   mapFilters.classList.remove('.map__filters--disabled');
+  mapFiltersSelect.forEach((select) => select.disabled = false);
+  mapFiltersFieldset.disabled = false;
 };
 
-export {getInactiveState, getActiveState};
+export {setInactiveState, setActiveState};
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {generateCard} from  './generate-card.js';
 import {similarAddOffers} from './data.js';
-import {getInactiveState, getActiveState} from './form.js';
+import {setInactiveState, setActiveState} from './form.js';
 
 
 const showCards = similarAddOffers(10);
@@ -8,5 +8,5 @@ const cards = showCards.map((publication) => generateCard(publication));
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(cards[0]);
 
-getInactiveState();
-getActiveState();
+setInactiveState();
+setActiveState();
