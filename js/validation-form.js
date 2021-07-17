2a786afc41a00d3826834b1a362741bf2d48a96f
ChrisNikulina <77628057+ChrisNikulina@users.noreply.github.com>
2021-07-17 16:41:12 +0300

    внесла правки по замечаниям

--- js/validation-form.js
+++ js/validation-form.js
@@ -6,13 +6,22 @@ const MIN_PRICE = {
   palace: 10000,
 };
 
-const MAX_ROOMS = 100;
+const ROOMS_CAPACITY = {
+  1 : ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0'],
+};
+
 
 const adForm = document.querySelector('.ad-form');
 const price = adForm.querySelector('#price');
 const typeOfApartament = adForm.querySelector('#type');
 const capacityOfGuests = document.querySelector('#capacity');
 const roomNumber = adForm.querySelector('#room_number');
+const options = capacityOfGuests.querySelectorAll('option');
+let roomOptions;
+
 
 const checkTypeOfApartament = () => {
   const priceMin = MIN_PRICE[typeOfApartament.value];
@@ -20,30 +29,36 @@ const checkTypeOfApartament = () => {
   price.placeholder = priceMin;
 };
 
+checkTypeOfApartament();
+
 typeOfApartament.addEventListener('change', () => {
   const typeValue = typeOfApartament.value;
   price.min = MIN_PRICE[typeValue];
   price.placeholder = MIN_PRICE[typeValue];
 });
 
-
-const checkRoomsValidity =  () => {
-  if (+capacityOfGuests.value > +roomNumber.value || (
-    +capacityOfGuests.value === 0 && +roomNumber.value < MAX_ROOMS) || (
-    +capacityOfGuests.value > 0 && +roomNumber.value === MAX_ROOMS )) {
+const capacityChangeValidity =  () => {
+  roomOptions = ROOMS_CAPACITY[roomNumber.value];
+  if (roomOptions.indexOf(capacityOfGuests.value) === -1) {
     capacityOfGuests.setCustomValidity('Количество гостей в выбранную комнату не подходит');
   } else {
     capacityOfGuests.setCustomValidity('');
   }
-  capacityOfGuests.reportValidity();
 };
 
-checkRoomsValidity();
-
-const onRoomsNumber = () => {
-  checkRoomsValidity();
+const roomNumbersChange = () => {
+  roomOptions = ROOMS_CAPACITY[roomNumber.value];
+  options.forEach((option) => {
+    if(roomOptions.indexOf(option.value) === -1) {
+      option.setAttribute('hidden', '');
+    } else {
+      option.removeAttribute('hidden');
+    }
+  });
+  capacityChangeValidity();
 };
 
-roomNumber.addEventListener('change', onRoomsNumber);
 
-checkTypeOfApartament();
+roomNumber.addEventListener('change',  roomNumbersChange);
+capacityOfGuests.addEventListener('change', capacityChangeValidity);
+
